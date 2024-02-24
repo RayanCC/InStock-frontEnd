65d70d016c58b6b49403c2d80388dd7142bdc0e0
KingChoices <kingchoicesyt@gmail.com>
2024-02-24 14:32:50 -0500

    - mobile styling completed - few changes in mixins

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Warehouse from "./components/Warehouse/Warehouse";
+import "./styles/global.scss";
+
 function App() {
   return (
     <div className="app">
       <p>Hello</p>
+      <Warehouse
+        warehouse={"Warehouse"}
+        address_line_1={"1"}
+        address_line_2={"2"}
+        contact={"us"}
+        number={"123"}
+        email={"123@today.tom"}
+      />
     </div>
   );
 }
