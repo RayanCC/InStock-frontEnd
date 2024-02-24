d42cbc0f111411ef002557cde902b782cae61865
KingChoices <kingchoicesyt@gmail.com>
2024-02-24 11:11:46 -0500

    - routes added for inventory and warehouse page - inventory and warehouse page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Warehouses from "./pages/Warehouses/Warehouses";
+import Inventory from "./pages/Inventory/Inventory";
+
 function App() {
   return (
-    <div className="app">
-      <p>Hello</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Warehouses />} />
+        <Route path="/inventory" element={<Inventory />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
