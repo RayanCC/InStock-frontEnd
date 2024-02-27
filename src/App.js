8ac496cf51622cf2b80cec064bd19487bec4ed15
KingChoices <81588553+KingChoices@users.noreply.github.com>
2024-02-27 15:31:57 -0500

    Revert "Revert 9 feature/edit warehouse"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Warehouses from "./pages/Warehouses/Warehouses";
+import Inventory from "./pages/Inventory/Inventory";
+import Warehouse from "./components/Warehouse/Warehouse";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import "./styles/global.scss";
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Warehouses />} />
+        <Route path="/inventory" element={<Inventory />} />
+      </Routes>
+      <Warehouse />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Warehouses/Warehouses.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Warehouses = () => {
+  return (
+    <div>
+      <h1>Warehouses</h1>
+    </div>
+  );
+};
+
+export default Warehouses;
