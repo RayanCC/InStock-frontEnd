59e3c208088eb51922d28b6f6b8d96bed5897889
albinsony95 <albinsony8@gmail.com>
2024-03-02 11:10:15 -0600

    Inventory details component completed

--- src/components/InventoryDetails/InventoryDetails.js
+++ src/components/InventoryDetails/InventoryDetails.js
@@ -12,16 +12,16 @@ import WarehouseInventory from '../WarehouseInventory/WarehouseInventory'
 import { useNavigate } from "react-router-dom"
 
 export const InventoryDetails = ({inventoryId}) => {
-    const navigate = useNavigate();
-                                        //const params=useParams();
-                                        //const =params.inventoryId ? params.inventoryId : inventoryId;    
     const[selectedInventory,setSelectedInventory] = useState([]);
+    const [statusColor, setStatusColor] = useState();
+    const navigate = useNavigate();
+
     const url=`http://localhost:5050/instock/inventory/${inventoryId}`;
+
     useEffect(() => {
         if(inventoryId){
             axios.get(url).then((res) => {
                 setSelectedInventory(res.data);
-                console.log(Inventory);
             }).catch((error) => {
                 console.log(error);
             });
@@ -30,6 +30,16 @@ export const InventoryDetails = ({inventoryId}) => {
             console.log("no Ids found");
         }
     },[inventoryId, url]);
+    useEffect(() => {
+        if (selectedInventory.length > 0) {
+            const inventoryStatus = selectedInventory[0].status;
+            if (inventoryStatus === 'In Stock') {
+            setStatusColor('green');
+            } else {
+            setStatusColor('red');
+            }
+        }
+    }, [selectedInventory]);
     if(!selectedInventory || selectedInventory.length ===0){
         return('');
     }
@@ -37,35 +47,43 @@ export const InventoryDetails = ({inventoryId}) => {
         navigate(-1);
     };
     const Inventory = selectedInventory[0];
+
   return (
-    <section className="detailsContainer" >
-        <section className='detailsContainer__headSection'>
-            <article className='detailsContainer__headSection-warehouseBox'>
-                <div className='detailsContainer__headSection-warehouseNameBox' onClick={goBack}>
-                    <img className='detailsContainer__headSection-warehouseNameBox--arrow' src={arrowBackIcon}/>
-                    <h1 className='detailsContainer__headSection-warehouseNameBox--name'>{Inventory.item_name}</h1>
+    <section className="inventoryContainer" >
+        <section className='inventoryContainer__headSection'>
+            <article className='inventoryContainer__headSection-InventoryBox'>
+                <div className='inventoryContainer__headSection-InventoryNameBox' onClick={goBack}>
+                    <img className='inventoryContainer__headSection-InventoryNameBox--arrow' src={arrowBackIcon}/>
+                    <h1 className='inventoryContainer__headSection-InventoryNameBox--name'>{Inventory.item_name}</h1>
                 </div>
-                <div className='detailsContainer__headSection-warehouseNameBox-editbox'>
-                    <img className='detailsContainer__headSection-warehouseNameBox--edit' src= {editIcon}/>
+                <div className='inventoryContainer__headSection-InventoryNameBox-editbox'>
+                    <svg className='inventoryContainer__headSection-InventoryNameBox--edit' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#2E66E6"/>
+                    </svg>
+                    <p className='inventoryContainer__headSection-InventoryNameBox-editbox-text'>Edit</p>
                 </div>
             </article>
-            <section className='detailsContainer__headSection-warehouseDetails'>
-                <article className='detailsContainer__headSection-warehouseDetails-addressInfo'>
-                    <h5 className='detailsContainer__headSection-warehouseDetails-info-lbl'>WAREHOUSE ADDRESS</h5>
-                    <span className='detailsContainer__headSection-warehouseDetails-addressInfo--address'>{Inventory.description}, </span>
-                    <span className='detailsContainer__headSection-warehouseDetails-addressInfo--city'>{Inventory.city}, </span>
-                    <span className='detailsContainer__headSection-warehouseDetails-addressInfo--country'>{Inventory.category}</span>
+            <section className='inventoryContainer__headSection-inventoryDetails'>
+                <article className='inventoryContainer__headSection-InventoryDetails-itemInfo'>
+                    <h5 className='inventoryContainer__headSection-InventoryDetails-info-lbl'>ITEM DESCRIPTION:</h5>
+                    <p className='inventoryContainer__headSection-InventoryDetails-itemInfo--description'>{Inventory.description}, </p>
+                    <h5 className='inventoryContainer__headSection-InventoryDetails-info-lbl'>CATEGORY:</h5>
+                    <p className='inventoryContainer__headSection-InventoryDetails-itemInfo--category'>{Inventory.category}</p>
                 </article>
-                <article className='detailsContainer__headSection-warehouseDetails-contactInfoBox'>
-                    <article className='detailsContainer__headSection-warehouseDetails-contactInfoBox-contactInfo'>
-                        <h5 className='detailsContainer__headSection-warehouseDetails-info-lbl'>CONTACT NAME</h5>
-                        <p className='detailsContainer__headSection-warehouseDetails-contactInfoBox-contactNameInfo--contactName'>{Inventory.status}</p>
-                        <p className='detailsContainer__headSection-warehouseDetails-ContactInfoBox--city'>{Inventory.quantity}</p>
+                <article className='inventoryContainer__headSection-inventoryDetails-Box'>
+                    <article className='inventoryContainer__headSection-InventoryDetails-InfoBox'>
+                        <article className='inventoryContainer__headSection-InventoryDetails-InfoBox-Info'>
+                            <h5 className='inventoryContainer__headSection-InventoryDetails-info-lbl'>STATUS:</h5>
+                            <p className='inventoryContainer__headSection-InventoryDetails-InfoBox-Info--status statusColour' style={{ color: statusColor }}>{Inventory.status}</p>
+                        </article>
+                        <article className='inventoryContainer__headSection-InventoryDetails-InfoBox-Info'>
+                            <h5 className='inventoryContainer__headSection-InventoryDetails-info-lbl'>QUANTITY:</h5>
+                            <p className='inventoryContainer__headSection-InventoryDetails-InfoBox--quantity'>{Inventory.quantity}</p>
+                        </article>
                     </article>
-                    <article className='detailsContainer__headSection-warehouseDetails-contactInfoBox-contactInfo'>
-                        <h5 className='detailsContainer__headSection-warehouseDetails-info-lbl'>CONTACT INFORMATION</h5>
-                        {/* <p className='detailsContainer__headSection-warehouseDetails-contactInfoBox--contact'>{warehouse.contact_phone}</p>
-                        <p className='detailsContainer__headSection-warehouseDetails-contactInfoBox--contact'>{warehouse.contact_email}</p> */}
+                    <article className='inventoryContainer__headSection-InventoryDetails-contactInfoBox-contactInfo'>
+                        <h5 className='inventoryContainer__headSection-InventoryDetails-info-lbl'>WAREHOUSE:</h5>
+                        <p className='inventoryContainer__headSection-InventoryDetails-contactInfoBox--contact'>{Inventory.warehouse_name}</p>
                     </article>
                 </article>
             </section>
--- src/components/warehouse_detail/WarehouseDetails.js
+++ src/components/warehouse_detail/WarehouseDetails.js
@@ -44,7 +44,12 @@ return(
                     <h1 className='detailsContainer__headSection-warehouseNameBox--name'>{warehouse.warehouse_name}</h1>
                 </div>
                 <div className='detailsContainer__headSection-warehouseNameBox-editbox'>
-                    <img className='detailsContainer__headSection-warehouseNameBox--edit' src= {editIcon}/>
+                
+                <svg className='detailsContainer__headSection-warehouseNameBox--edit' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#2E66E6"/>
+</svg>
+<p className='detailsContainer__headSection-warehouseNameBox-editbox-text'>Edit</p>
+                    {/* <img className='detailsContainer__headSection-warehouseNameBox--edit' src= {editIcon}/> */}
                 </div>
             </article>
             <section className='detailsContainer__headSection-warehouseDetails'>
